f9d09b44a4ed3341ad8efdfee91534257206477c
Tomás Moreno <morenotomas2112@gmail.com>
2026-06-16 21:00:58 -0300

    fix: botones no respondian en android - mouse_entered/exited removido, enable_touch explicit

--- scenes/effects/button_feedback.gd
+++ scenes/effects/button_feedback.gd
@@ -3,21 +3,11 @@ extends Button
 func _ready() -> void:
 	button_down.connect(_on_down)
 	button_up.connect(_on_up)
-	mouse_entered.connect(_on_hover)
-	mouse_exited.connect(_on_exit)
 
 func _on_down() -> void:
 	var tween := create_tween()
 	tween.tween_property(self, "scale", Vector2(0.92, 0.92), 0.08)
 
 func _on_up() -> void:
-	var tween := create_tween()
-	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
-
-func _on_hover() -> void:
-	var tween := create_tween()
-	tween.tween_property(self, "scale", Vector2(1.05, 1.05), 0.08)
-
-func _on_exit() -> void:
 	var tween := create_tween()
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
